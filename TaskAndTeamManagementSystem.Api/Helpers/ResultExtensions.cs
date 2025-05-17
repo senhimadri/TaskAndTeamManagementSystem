009f0ed957d2e11a456088a3fe93ecb0ca2abf8d
senhimadri <senhimadri.dev@gmail.com>
2025-05-17 16:13:44 +0600

    feat(Task item ): Controller Created

---
+++ TaskAndTeamManagementSystem.Api/Helpers/ResultExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskAndTeamManagementSystem.Application.Helpers.Results;
+
+namespace TaskAndTeamManagementSystem.Api.Helpers;
+
+public static class ResultExtensions
+{
+    public static IActionResult Match(this Result result,
+        Func<IActionResult> onSuccess,
+        Func<ValidationProblemDetails, IActionResult> onValidationFailure,
+        Func<Error, IActionResult> onFailure)
+    {
+        return result.IsSuccess ? onSuccess()
+            : (result.IsValidationFailure && result.ValidationErrors is not null) ? onValidationFailure(result.ValidationErrors.ToValidationDetails())
+            : onFailure(result.Error);
+    }
+}
