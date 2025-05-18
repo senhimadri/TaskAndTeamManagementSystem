675475e2baec9cfa3e909272b17555bd70dffdca
senhimadri <senhimadri.dev@gmail.com>
2025-05-18 06:53:55 +0600

    feat(Task Item Details) : Small update in task item details

--- TaskAndTeamManagementSystem.Application/Contracts/Persistences/IRepositories/ITaskItemRepository.cs
+++ TaskAndTeamManagementSystem.Application/Contracts/Persistences/IRepositories/ITaskItemRepository.cs
@@ -5,8 +5,9 @@ namespace TaskAndTeamManagementSystem.Application.Contracts.Persistences.IReposi
 
 public interface ITaskItemRepository : IGenericRepository<TaskItem, long>
 {
-    public IQueryable<TaskItem> TaskItemDetails(Expression<Func<TaskItem, bool>> filter,
-                                                Expression<Func<TaskItem, object>>? orderBy = null,
+    public IQueryable<TaskItem> TaskItemDetails<TKey>(Expression<Func<TaskItem, bool>> filter,
+                                                Expression<Func<TaskItem, TKey>>? orderBy = null,
                                                 bool ascending = true);
+    public IQueryable<TaskItem> TaskItemDetails(Expression<Func<TaskItem, bool>> filter);
 }
 
--- TaskAndTeamManagementSystem.Persistence/Repositories/TaskItemRepository.cs
+++ TaskAndTeamManagementSystem.Persistence/Repositories/TaskItemRepository.cs
@@ -9,20 +9,28 @@ internal class TaskItemRepository(AppDbContext context) : GenericRepository<Task
 {
     private readonly AppDbContext _context = context;
 
-    public IQueryable<TaskItem> TaskItemDetails(Expression<Func<TaskItem, bool>> filter,
-                                                Expression<Func<TaskItem, object>>? orderBy = null,
+    public IQueryable<TaskItem> TaskItemDetails<TKey>(Expression<Func<TaskItem, bool>> filter,
+                                                Expression<Func<TaskItem, TKey>>? orderBy = null,
                                                 bool ascending = true)
+    {
+        var query = TaskItemDetails(filter);
+
+        if (orderBy != null)
+        {
+            query = ascending ? query.OrderBy(orderBy) : query.OrderByDescending(orderBy);
+        }
+
+        return query.AsQueryable();
+    }
+
+
+    public IQueryable<TaskItem> TaskItemDetails(Expression<Func<TaskItem, bool>> filter)
     {
         var query = _context.TaskItems
             .Include(t => t.AssignedUser)
             .Include(t => t.CreatedByUser)
             .Include(t => t.Team)
             .Where(filter);
-
-        if (orderBy != null)
-        {
-            query = ascending ? query.OrderBy(orderBy) : query.OrderByDescending(orderBy);
-        }
 
         return query.AsQueryable();
     }
